883bfcf41dc7826c4cf53a676dacfeef11f8b658
totori <to_asdf@yahoo.co.jp>
2022-08-11 20:47:13 +0900

    update visualizer

--- src/bin/vis.rs
+++ src/bin/vis.rs
@@ -1,8 +1,23 @@
 use tools::*;
 
+#[derive(Clone, Debug)]
+pub struct Answer {
+    pub n: usize,
+    pub k: usize,
+    pub svg: String,
+}
+
+
 fn main() {
 
     let mut doc = "".to_string();
+    let mut v = Vec::new();
+
+    
+    // TODO: 
+    // - Kごととかで表示
+    // - 何件かまとめて表示
+    
 
     for i in 0..100{
         let in_file = format!("in/{:>04}.txt", i);
@@ -20,6 +35,8 @@ fn main() {
         let input = parse_input(&input);
         let output = parse_output(&input, &output);
 
+        // println!("{}", input.n);
+
         let (_, err, svg) = match output {
             Ok(output) => vis(&input, &output),
             Err(err) => (0, err, String::new()),
@@ -28,12 +45,29 @@ fn main() {
         if err.len() > 0 {
             println!("{}", err);
         }
+        v.push(Answer {n : input.n.clone(), k : input.k.clone(), svg : svg.clone()});
+        // v.push(String::from(svg));
         let vis = format!("<html><body>{}</body></html>", svg);
         std::fs::write(format!("visualize/{}.html", basename), &vis).unwrap();
 
         doc = format!("{}{}", doc, format!("<a href=\"./visualize/{}.html\">{}</a>\n", basename, basename))
     }
 
-    let index = format!("<html><body>{}</body></html>", doc);
+    let mut index = format!("<html><body>{}<br><br>", doc);
+
+    let sz = v.len() / 10;
+    for i in 0..sz {
+        let mut clus = "".to_string();
+        let l = i * 10;
+        let r = std::cmp::min(v.len(), i*10+10);
+        for j in l..r {
+            clus = format!("{}seed={}, N={}, K={}<br>{}<br><br>", clus, j, v[j].n, v[j].k, v[j].svg);
+        }
+        let vis = format!("<html><body>{}</body></html>", clus);
+        std::fs::write(format!("visualize/clus_{}.html", i), &vis).unwrap();
+        index = format!("{}{}", index, format!("<a href=\"./visualize/clus_{}.html\">{}-{}</a>\n", i, l, r));
+    }
+
+    index = format!("{}{}", index, "</body></html>");
     std::fs::write("index.html", &index).unwrap();
 }
