05dcd54497e57572a9d52b1acb55d6ddd9b8304d
totori <to_asdf@yahoo.co.jp>
2022-08-11 17:46:50 +0900

    visualize

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 cd `dirname $0`
 g++ -std=gnu++1z -I . -O2 -Wall -Wfatal-errors -Wextra -W main.cpp
 # g++ calc_score.cpp -o calc_score.out
-# rm -rf score
-# rm -rf out
-# mkdir score
-# mkdir out
+rm -rf score
+rm -rf out
+rm -rf visualize
+mkdir score
+mkdir out
+mkdir visualize
 
 st=0
 en=0
@@ -22,14 +24,16 @@ export -f calc
 
 # -J : 100ケース実行
 while getopts "j:J" optKey; do
-  case "$optKey" in
-    J)
-      en=99
-      slep=4
-      ;;
-  esac
+  	case "$optKey" in
+    	J)
+			en=99
+			slep=3
+			;;
+  	esac
 done
 
 seq -f '%04g' $st $en | xargs -n1 -P $procs -I{} bash -c "calc {}" & sleep $slep
+
+cargo run --release --bin vis
 
 python3 score.py
--- src/bin/vis.rs
+++ src/bin/vis.rs
@@ -1,36 +1,32 @@
 use tools::*;
 
 fn main() {
-    if std::env::args().len() != 3 {
-        eprintln!(
-            "Usage: {} <input> <output>",
-            std::env::args().nth(0).unwrap()
-        );
-        return;
+
+    for i in 0..100{
+        let in_file = format!("in/{:>04}.txt", i);
+        let out_file = format!("out/{:>04}.txt", i);
+        let basename = format!("{:>04}", i);
+        let input = std::fs::read_to_string(&in_file).unwrap_or_else(|_| {
+            eprintln!("no such file: {}", in_file);
+            std::process::exit(1);
+        });
+        let output = std::fs::read_to_string(&out_file).unwrap_or_else(|_| {
+            eprintln!("no such file: {}", out_file);
+            std::process::exit(1);
+        });
+
+        let input = parse_input(&input);
+        let output = parse_output(&input, &output);
+
+        let (_, err, svg) = match output {
+            Ok(output) => vis(&input, &output),
+            Err(err) => (0, err, String::new()),
+        };
+
+        if err.len() > 0 {
+            println!("{}", err);
+        }
+        let vis = format!("<html><body>{}</body></html>", svg);
+        std::fs::write(format!("visualize/{}.html", basename), &vis).unwrap();
     }
-    let in_file = std::env::args().nth(1).unwrap();
-    let out_file = std::env::args().nth(2).unwrap();
-    let input = std::fs::read_to_string(&in_file).unwrap_or_else(|_| {
-        eprintln!("no such file: {}", in_file);
-        std::process::exit(1);
-    });
-    let output = std::fs::read_to_string(&out_file).unwrap_or_else(|_| {
-        eprintln!("no such file: {}", out_file);
-        std::process::exit(1);
-    });
-
-    let input = parse_input(&input);
-    let output = parse_output(&input, &output);
-
-    let (score, err, svg) = match output {
-        Ok(output) => vis(&input, &output),
-        Err(err) => (0, err, String::new()),
-    };
-
-    println!("Score = {}", score);
-    if err.len() > 0 {
-        println!("{}", err);
-    }
-    let vis = format!("<html><body>{}</body></html>", svg);
-    std::fs::write("vis.html", &vis).unwrap();
 }
